a0e40731f17c66c45d7d954b2e9143b8b51f1510
Chris Farris <chris.farris@turner.com>
2019-07-02 14:46:10 -0400

    add aws_profile function to do the environment and prompt override for stuff in the credentail or config file

--- bin/aws_account.sh
+++ bin/aws_account.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script will pull API keys from OSX Keychain and allow you to easily manage multiple accounts. 
+# This script will pull API keys from OSX Keychain and allow you to easily manage multiple accounts.
 
 # Each account needs a definition in ~/.aws/config
 # specifying the region is optional
@@ -58,6 +58,41 @@ aws_account () {
 	export COLOR="32m"
 	export PS1="\[\033[$COLOR\][\u@\h \W] $AWSUSER@$AWS_DEFAULT_PROFILE ($AWS_DEFAULT_REGION):\[\033[0m\] "
 }
+
+aws_profile () {
+	if [ -z $1 ] ; then
+	  echo "Usage: aws_account <aws_profile> <region>"
+	  return 1
+	fi
+
+	# Escape the ][ lest you are using a regex. eek
+	grep "\[profile $1\]" ~/.aws/config > /dev/null 2>&1
+	if [ $? != 0 ] ; then
+	  echo "Invalid profile. Check the files in ~/.aws/config"
+	  return 1
+	fi
+
+	echo "Using $1 as my AWS Account"
+	# Allow you to not need that dang --profile on each command
+	export AWS_DEFAULT_PROFILE=$1
+	export AWS_PROFILE=$1
+
+	if [ ! -z "$2" ] ; then
+		export AWS_DEFAULT_REGION="$2"
+	else
+		export AWS_DEFAULT_REGION="us-east-1"
+	fi
+
+	# Remove any existing environment keys
+	unset AWS_SECRET_ACCESS_KEY
+	unset AWS_ACCESS_KEY_ID
+	unset AWS_SESSION_TOKEN
+
+	# Set the prompt so you know what you're doing
+	export COLOR="94m"
+	export PS1="\[\033[$COLOR\][\u@\h \W] $AWSUSER@$AWS_DEFAULT_PROFILE ($AWS_DEFAULT_REGION):\[\033[0m\] "
+}
+
 
 aws_mfa () {
 	if [ -z $1 ] ; then
