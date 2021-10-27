6d95937fea786643ca3e1f859b1bac51355f32da
Chris Farris <chris_farris@discovery.com>
2021-10-27 07:41:21 -0400

    Script to delete all versions and delete markers in a bucket.

---
+++ sbin/burn-bucket-to-ground.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+buckettoempty=$1
+
+if [ -z "$buckettoempty" ] ; then
+	echo "Usage: $0 <buckettoempty>"
+	exit 1
+fi
+
+# 1: Delete objects
+aws s3api delete-objects --bucket ${buckettoempty} --delete "$(aws s3api list-object-versions --bucket ${buckettoempty} --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"
+
+# 2: Delete markers
+aws s3api delete-objects --bucket ${buckettoempty} --delete "$(aws s3api list-object-versions --bucket ${buckettoempty} --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')"
+
+# 3. Delete Bucket
+aws s3 rb s3://${buckettoempty}/ --force
